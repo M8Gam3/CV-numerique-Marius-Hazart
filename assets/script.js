8fbb9ea9f5aa0d1ec57c7164f1c8aae4e868ea12
Marius Hazart <115085694+M8Gam3@users.noreply.github.com>
2023-03-08 16:43:20 +0100

    push

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,57 @@
-console.log("bonjour");
+Vue.component('presentation-div', {
+    template:   `<div class="presentation-article">
+                    <h3> {{presentation_title}} </h3>
+                    <p> {{presentation_text}} </p>
+                </div>`,
+    props: ['presentation_title', 'presentation_text'],
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        home: false,
+        portfolio: false,
+        presentation: true,
+        skills: false,
+        presentationArticle: [
+            {
+                title: "La Manu",
+                desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur ut ex eget efficitur. Cras odio nisi, venenatis auctor imperdiet non, efficitur ac enim. Donec aliquet feugiat eleifend.",
+            },
+            {
+                title: "Edouard Branly",
+                desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur ut ex eget efficitur. Cras odio nisi, venenatis auctor imperdiet non, efficitur ac enim. Donec aliquet feugiat eleifend.",
+            },
+        ]
+
+    },
+    computed: {
+
+    },
+    methods: {
+        navHome: function () {
+            this.home = true;
+            this.portfolio = false;
+            this.presentation = false;
+            this.skills = false;
+        },
+        navPortfolio: function () {
+            this.home = false;
+            this.portfolio = true;
+            this.presentation = false;
+            this.skills = false;
+        },
+        navPresentation: function () {
+            this.home = false;
+            this.portfolio = false;
+            this.presentation = true;
+            this.skills = false;
+        },
+        navSkills: function () {
+            this.home = false;
+            this.portfolio = false;
+            this.presentation = false;
+            this.skills = true;
+        },
+    },
+})
