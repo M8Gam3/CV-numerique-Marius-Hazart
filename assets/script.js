68e6485e18998b48158c33119cd041fab9aa33ce
Marius Hazart <115085694+M8Gam3@users.noreply.github.com>
2023-03-09 16:43:06 +0100

    portfolio

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,14 @@ Vue.component('presentation-div', {
     props: ['presentation_title', 'presentation_text'],
 });
 Vue.component('portfolio-div', {
-    template:   `<div class="portfolio-article">
-                    <h3> {{portfolio_article_title}} lorem ipsum dolor sit amet. </h3>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png" alt="">
-                    <button>0-0</button>
+    template:   `<div class="portfolio-projet">
+                    <h3> {{title}} </h3>
+                    <img v-bind:src="image" alt="">
+                    <a v-bind:href="link"> 
+                        <button> {{button}} </button>
+                    </a>
                 </div>`,
-    props: [],
+    props: ['title', 'image', 'button', 'link'],
 });
 
 var app = new Vue({
@@ -34,7 +36,33 @@ var app = new Vue({
                 title: "Novalis",
                 desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur ut ex eget efficitur. Cras odio nisi, venenatis auctor imperdiet non, efficitur ac enim. Donec aliquet feugiat eleifend.",
             },
-        ]
+        ],
+        portfolioProjets: [
+            {
+                title: "Projet Shifumi",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+                button: "Lien du site",
+                link: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+            },
+            {
+                title: "Projet Restaurant",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+                button: "Lien du site",
+                link: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+            },
+            {
+                title: "Projet Instant Gaming",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+                button: "Lien du site",
+                link: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+            },
+            {
+                title: "Projet CV",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+                button: "Lien du Figma",
+                link: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+            },
+        ],
 
     },
     computed: {
@@ -67,3 +95,18 @@ var app = new Vue({
         },
     },
 })
+
+
+// {
+//     name: 'css',
+//     type:[
+//         {
+//             name: 'flex',
+//             niveau: 4,
+//         },
+//         {
+//             name: 'grid',
+//             niveau: 1
+//         }
+//     ],
+// },
